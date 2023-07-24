7917311e522f7178b690edc0b458c0d518224ce6
nada105 <35874285+nada105@users.noreply.github.com>
2023-07-25 00:28:01 +0300

    Add files via upload

---
+++ src/page/bar/BarChart.jsx
@@ -0,0 +1,271 @@
+
+import { Box, useTheme } from '@mui/material';
+import { ResponsiveBar } from '@nivo/bar';
+
+
+const data=[
+    {
+      year:2019,
+      spain:900,
+      France:1000,
+      Germany:700,
+    },
+    {
+      year:2020,
+      spain:500,
+      France:100,
+      Germany:1000,
+    },
+    {
+      year:2021,
+      spain:1200,
+      France:2000,
+      Germany:1200,
+    },
+    {
+      year:2022,
+      spain:2000,
+      France:800,
+      Germany:1500,
+    },
+    {
+      year:2023,
+      spain:2500,
+      France:1300,
+      Germany:1700,
+    }
+  ]
+
+
+
+
+
+ const BarChart = () => {
+    const theme =useTheme()
+  return (
+   <Box sx={{height:"75vh"}}>
+    <ResponsiveBar
+      data={data}
+      keys={['spain','France','Germany']}
+
+      // INDEXbY de x axis//
+      indexBy="year"
+      // You can pass this object to the `theme` property
+      theme={{
+
+                fontSize: 11,
+                textColor:"#333333",
+               
+
+            "axis": {
+                "domain": {
+                    "line": {
+                        "stroke": theme.palette.divider,
+                        "strokeWidth": 1
+                    }
+                },
+                "legend": {
+                    "text": {
+                        "fontSize": 12,
+                        "fill": theme.palette.text.primary,
+                        "outlineWidth": 0,
+                        "outlineColor": "transparent"
+                    }
+                },
+                "ticks": {
+                    "line": {
+                        "stroke": theme.palette.divider,
+                        "strokeWidth": 1
+                    },
+                    "text": {
+                        "fontSize": 11,
+                        "fill": theme.palette.text.primary,
+                        "outlineWidth": 0,
+                        "outlineColor": "transparent"
+                    }
+                }
+            },
+            "grid": {
+                "line": {
+                    "stroke": theme.palette.divider,
+                    "strokeWidth": 1
+                }
+            },
+            "legends": {
+                "title": {
+                    "text": {
+                        "fontSize": 11,
+                        "fill": theme.palette.text.primary,
+                        "outlineWidth": 0,
+                        "outlineColor": "transparent"
+                    }
+                },
+                "text": {
+                    "fontSize": 11,
+                    "fill": theme.palette.text.primary,
+                    "outlineWidth": 0,
+                    "outlineColor": "transparent"
+                },
+                "ticks": {
+                    "line": {},
+                    "text": {
+                        "fontSize": 10,
+                        "fill": theme.palette.text.primary,
+                        "outlineWidth": 0,
+                        "outlineColor": "transparent"
+                    }
+                }
+            },
+            "annotations": {
+                "text": {
+                    "fontSize": 13,
+                    "fill": theme.palette.text.primary,
+                    "outlineWidth": 2,
+                    "outlineColor": "#ffffff",
+                    "outlineOpacity": 1
+                },
+                "link": {
+                    "stroke": "#000000",
+                    "strokeWidth": 1,
+                    "outlineWidth": 2,
+                    "outlineColor": "#ffffff",
+                    "outlineOpacity": 1
+                },
+                "outline": {
+                    "stroke": "#000000",
+                    "strokeWidth": 2,
+                    "outlineWidth": 2,
+                    "outlineColor": "#ffffff",
+                    "outlineOpacity": 1
+                },
+                "symbol": {
+                    "fill": "#000000",
+                    "outlineWidth": 2,
+                    "outlineColor": "#ffffff",
+                    "outlineOpacity": 1
+                }
+            },
+            "tooltip": {
+                "container": {
+                    "background": theme.palette.background.default,
+                    "fontSize": 12
+                },
+                "basic": {},
+                "chip": {},
+                "table": {},
+                "tableCell": {},
+                "tableCellValue": {}
+            }
+        }}
+
+
+
+      margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
+      padding={0.3}
+      valueScale={{ type: 'linear' }}
+      indexScale={{ type: 'band', round: true }}
+      colors={{ scheme: 'paired' }}
+      defs={[
+          {
+              id: 'dots',
+              type: 'patternDots',
+              background: 'inherit',
+              color: '#38bcb2',
+              size: 4,
+              padding: 1,
+              stagger: true
+          },
+          {
+              id: 'lines',
+              type: 'patternLines',
+              background: 'inherit',
+              color: '#eed312',
+              rotation: -45,
+              lineWidth: 6,
+              spacing: 10
+          }
+      ]}
+      fill={[
+          {
+              match: {
+                  id: 'fries'
+              },
+              id: 'dots'
+          },
+          {
+              match: {
+                  id: 'sandwich'
+              },
+              id: 'lines'
+          }
+      ]}
+      borderColor={{
+          from: 'color',
+          modifiers: [
+              [
+                  'darker',
+                  1.6
+              ]
+          ]
+      }}
+      axisTop={null}
+      axisRight={null}
+      axisBottom={{
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          legend: 'year',
+          legendPosition: 'middle',
+          legendOffset: 35
+      }}
+      axisLeft={{
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          legend: 'sallary/month',
+          legendPosition: 'middle',
+          legendOffset: -50
+      }}
+      labelSkipWidth={12}
+      labelSkipHeight={12}
+      labelTextColor={{
+          from: 'color',
+          modifiers: [
+              [
+                  'darker',
+                  1.6
+              ]
+          ]
+      }}
+      legends={[
+          {
+              dataFrom: 'keys',
+              anchor: 'bottom-right',
+              direction: 'column',
+              justify: false,
+              translateX: 120,
+              translateY: 0,
+              itemsSpacing: 2,
+              itemWidth: 100,
+              itemHeight: 20,
+              itemDirection: 'left-to-right',
+              itemOpacity: 0.85,
+              symbolSize: 20,
+              effects: [
+                  {
+                      on: 'hover',
+                      style: {
+                          itemOpacity: 1
+                      }
+                  }
+              ]
+          }
+      ]}
+      role="application"
+      ariaLabel="Nivo bar chart demo"
+      barAriaLabel={e=>e.id+": "+e.formattedValue+" in country: "+e.indexValue}
+  />
+  </Box>
+  )
+}
+export default BarChart;
